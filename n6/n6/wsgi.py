cd15a773e6ac68738aec18277d738d7503afe40f
Priyank Maniya <priyankmaniya199@gmail.com>
2023-04-17 16:14:37 -0600

    path changed

--- n6/n6/wsgi.py
+++ n6/n6/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'n6/n6.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'n6.n6.settings')
 
 application = get_wsgi_application()
